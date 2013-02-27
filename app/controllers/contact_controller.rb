1289f46cdc090a0f1443cf79e04ba714df39574c
Maxim Kadyrov <maxkadyrov@gmail.com>
2013-02-27 11:50:58 +0400

    Fixed img pathes and optimized some code.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -9,17 +9,17 @@ class ContactController < ApplicationController
     days = (diff - years * 365*60*60*24 - months*30*60*60*24)/ (60*60*24) # XXXX.XX.XX - years - months = days
 
     if years != 0
-      str = str + years.to_s + ' years '
+      str += years.to_s + ' years '
     end
     if months != 0
-      str = str + months.to_s + ' months '
+      str += months.to_s + ' months '
     end
     if days == 1
-      str = str + days.to_s + ' day ago'
+      str += days.to_s + ' day ago'
     elsif days != 0
-      str = str + days.to_s + ' days ago'
+      str += days.to_s + ' days ago'
     else
-      str = str + 'less than 1 day ago'
+      str += 'less than 1 day ago'
     end
 
     result = {
@@ -55,17 +55,17 @@ class ContactController < ApplicationController
       html_code += '<div class="name_container">'
       html_code += '<div class="contact_name" id="contact_name_' + c.id.to_s + '">' + c.name.to_s + '</div>'
       html_code += '<div class="contact_edit" id="contact_edit_' + c.id.to_s + '">'
-      html_code += '<img src="edit.png"></img>' 
+      html_code += '<img src="/assets/edit.png"></img>' 
       html_code += '</div>'
       html_code += '<div class="contact_delete" id="contact_delete_' + c.id.to_s + '">'
-      html_code += '<img src="no.png"></img>' 
+      html_code += '<img src="/assets/no.png"></img>' 
       html_code += '</div>'
       html_code += '</div>'
       # ===/name_container===
       # ===contact_date===
       html_code += '<div class="contact_date_diff">'
       html_code += '<div class="contact_update" id="contact_update_' + c.id.to_s + '">'
-      html_code += '<img src="update.png"></img>' 
+      html_code += '<img src="/assets/update.png"></img>' 
       html_code += '</div>'
       html_code += date_array["date_diff"]
       html_code += '</div>'
